0fcfb1e6f7a1f7afb0f0d50ca1000db8ce1bf1d0
melgohary2715 <melgohary2715@gmail.com>
2024-03-25 11:46:52 -0400

    fixed pics

--- PlatformerTutorial/src/utilz/LoadSave.java
+++ PlatformerTutorial/src/utilz/LoadSave.java
@@ -12,8 +12,8 @@ import main.Game;
 public class LoadSave {
 
 	public static final String PLAYER_ATLAS = "player_sprites.png";
-	public static final String LEVEL_ATLAS = "outside_sprites.png";
-	public static final String LEVEL_ONE_DATA = "level_one_data.png";
+	public static final String LEVEL_ATLAS = "res/outside_sprites.png";
+	public static final String LEVEL_ONE_DATA = "res/level_one_data.png";
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
